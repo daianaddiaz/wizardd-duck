bc0cc402b65a3369d138ef872f5a3aba574f52ed
Daiana Daniela Diaz <80778668+daianaddiaz@users.noreply.github.com>
2025-11-18 20:38:21 -0300

    Add files via upload

---
+++ Idle (64x64)-sheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Idle (64x64)-sheet" class="Personaje" tilewidth="16" tileheight="16" tilecount="176" columns="44">
+ <image source="wizard-duck/Idle (64x64)-sheet.png" width="704" height="64"/>
+</tileset>
---
+++ castle-tileset.tsx
@@ -0,0 +1,137 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="castle-tileset" tilewidth="16" tileheight="16" tilecount="128" columns="8">
+ <image source="wizard-duck/castle-tileset.png" width="128" height="256"/>
+ <tile id="0">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="9">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="11">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="12">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="13">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="16">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="17">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="18">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="19">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="20">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="21">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+   <object id="2" x="0" y="0" width="16" height="16"/>
+   <object id="3" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="33" type="Plataforma">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="4.0625" y="0">
+    <polygon points="0,0 11.75,-0.0625 11.9375,10.6875 -0.0625,3.5625"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="34" type="Plataforma">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.25" y="-0.0625">
+    <polygon points="0,0 16.25,0.0625 16.25,10.9375 0.1875,11"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="35" type="Plataforma">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="0" y="11.0625">
+    <polygon points="0,0 11.9375,-7.3125 11.875,-11 -0.0625,-11.0625"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="36" type="Plataforma">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="12" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="37" type="Plataforma">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="12" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="38" type="Plataforma">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="12" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="57" type="Plataforma">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="0.0625" y="-0.0625" width="15.9375" height="16"/>
+  </objectgroup>
+ </tile>
+</tileset>
